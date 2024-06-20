97064fb606d50cdd8fc91d8d10b84019df45ab78
Lauri Juvela <lauri.juvela@aalto.fi>
2024-06-20 15:00:23 +0300

    add unit tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -18,6 +18,26 @@ def test_lfcc_lcnn():
 
     assert x.grad is not None
 
+
+def test_lfcc_lcnn_eval():
+        
+    from collaborative_watermarking.models.lfcc_lcnn import LFCC_LCNN
+
+    model = LFCC_LCNN(in_dim=1, out_dim=1, sample_rate=16000)
+    model.eval(pass_gradients=True)
+
+    batch = 2
+    timesteps = 16000
+    channels = 1
+    x = 0.1 * torch.randn(batch, 1, timesteps, requires_grad=True)
+    x = torch.nn.Parameter(x)
+
+    scores = model.forward(x)
+    # check that gradients pass
+    scores.sum().backward()
+
+    assert x.grad is not None
+
 def test_rawnet():
 
     from collaborative_watermarking.models.rawnet2 import RawNet2
@@ -39,4 +59,27 @@ def test_rawnet():
 
     assert x.grad is not None
 
+
+
+def test_rawnet_eval():
+
+    from collaborative_watermarking.models.rawnet2 import RawNet2
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    batch = 3
+    timesteps = 22050
+    channels = 1
+    x = 0.1 * torch.randn(batch, 1, timesteps, requires_grad=True)
+    x = torch.nn.Parameter(x)
+    x_dev = x.to(device)
+
+    model = RawNet2(sample_rate=16000)
+    model = model.to(device)
+    model.eval(pass_gradients=True)
+
+    scores = model.forward(x_dev)
+    scores.pow(2).sum().backward()
+
+    assert x.grad is not None
 
